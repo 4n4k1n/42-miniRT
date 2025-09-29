b45aa4efdeb687b17f5c47d0bdd6a8315fe71dc7
4n4k1n <anakin@pregitzer.de>
2025-09-29 19:58:49 +0200

    add:   main.c{idk tf}

--- src/main.c
+++ src/main.c
@@ -6,14 +6,18 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:00:01 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/29 12:14:05 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/09/29 19:27:35 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "miniRT.h"
+#include "mini_rt.h"
 
 int	main(int ac, char **av)
 {
+	t_data	data;
+	int	i;
+	int	j;
+	
 	(void)ac;
 	(void)av;
 
@@ -25,7 +29,16 @@ int	main(int ac, char **av)
 	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
 		return (1);
 
-	// add put pixel
+	i = 0;
+	while (i < data.height)
+	{
+		j = 0;
+		while (j < data.width)
+		{
+			
+		}
+	}
+
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 
