8525162251e4977e8339ca74072d32afb488b2c0
4n4k1n <anakin@pregitzer.de>
2025-09-29 22:08:21 +0200

    add: ray color function

--- src/utils/ray.c
+++ src/utils/ray.c
@@ -6,7 +6,7 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 16:54:31 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/29 21:34:54 by anakin           ###   ########.fr       */
+/*   Updated: 2025/09/29 22:04:39 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,24 @@ inline t_vec3	ray_at(t_ray *ray, double t)
 
 t_rgb	ray_color(t_ray *ray)
 {
-	t_rgb	color;
-
-	(void)ray;
-	color.r = 0;
-	color.g = 0;
-	color.b = 0;
-	return (color);
+	t_vec3	color_a = {1.0, 1.0, 1.0};
+	t_vec3	color_b = {0.5, 0.7, 1.0};
+	t_rgb	return_color;
+	t_vec3	unit_direction;
+	t_vec3	result;
+	t_vec3	temp1;
+	t_vec3	temp2;
+	double	a;
+	
+	unit_direction = vec3_cpy_inline(&ray->direction);
+	a = 0.5 * (unit_direction.y + 1.0);
+	temp1 = vec3_multiply_inline(&color_a, 1.0 - a);
+	temp2 = vec3_multiply_inline(&color_b, a);
+	result = vec3_add_inline(&temp1, &temp2);
+	return_color.r = result.x;
+	return_color.g = result.y;
+	return_color.b = result.z;
+	return (return_color);
 }
 
 uint32_t	rgb_to_uint32(t_rgb *color)
