cce21f0ec15cfaabb96beee3066bfb4a52034417
Niklas Weber <nweber@student.42Heilbronn.de>
2025-09-30 13:04:23 +0200

    refactored: hit_sphere function

--- src/logic/hit_sphere.c
+++ src/logic/hit_sphere.c
@@ -6,24 +6,26 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 11:32:34 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/30 12:37:52 by nweber           ###   ########.fr       */
+/*   Updated: 2025/09/30 12:59:25 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
 
-int	hit_sphere(t_vec3 center, double radius, t_ray *r)
+double	hit_sphere(t_vec3 center, double radius, t_ray *r)
 {
 	t_vec3	oc;
 	double	a;
-	double	b;
+	double	h;
 	double	c;
 	double	discriminant;
 
 	oc = vec3_sub_inline(&center, &r->origin);
 	a = vec3_dot_inline(&r->direction, &r->direction);
-	b = -2.0 * vec3_dot_inline(&r->direction, &oc);
+	h = vec3_dot_inline(&r->direction, &oc);
 	c = vec3_dot_inline(&oc, &oc) - radius * radius;
-	discriminant = b * b - 4.0 * a * c;
-	return (discriminant >= 0.0);
+	discriminant = h * h - a * c;
+	if (discriminant < 0.0)
+		return (-1.0);
+	return ((h - sqrt(discriminant)) / a);
 }
