daa5225bb243d0e9489baaf93065915bf3c4d595
Niklas Weber <nweber@student.42Heilbronn.de>
2025-09-30 17:22:28 +0200

    changed: main scene to render 2 spheres

--- src/main.c
+++ src/main.c
@@ -6,23 +6,59 @@
 /*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:00:01 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/30 12:38:19 by nweber           ###   ########.fr       */
+/*   Updated: 2025/09/30 17:07:00 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
 
+static void	build_demo_world(t_data *data)
+{
+	t_obj	*s1;
+	t_obj	*s2;
+
+	data->objects = (t_obj_list *)malloc(sizeof(t_obj_list));
+	if (!data->objects)
+		return;
+	data->objects->head = NULL;
+	data->objects->tail = NULL;
+	data->objects->size = 0;
+
+	// Sphere at (0,0,-1), radius 0.5 -> diameter 1.0
+	s1 = obj_new(SPHERE);
+	if (s1)
+	{
+		s1->data.sphere.cords = vec3_init_inline(0.0, 0.0, -1.0);
+		s1->data.sphere.diameter = 1.0;
+		s1->data.sphere.rgb = (t_rgb){255, 255, 255};
+		obj_push(data->objects, s1);
+	}
+
+	// Ground sphere at (0,-100.5,-1), radius 100 -> diameter 200
+	s2 = obj_new(SPHERE);
+	if (s2)
+	{
+		s2->data.sphere.cords = vec3_init_inline(0.0, -100.5, -1.0);
+		s2->data.sphere.diameter = 200.0;
+		s2->data.sphere.rgb = (t_rgb){255, 255, 255};
+		obj_push(data->objects, s2);
+	}
+}
+
 int	main(int ac, char **av)
 {
 	t_data	data;
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 
 	(void)ac;
 	(void)av;
 
 	// Initialize camera
 	init_camera(&data);
+
+	// Build a simple world of hittables (two spheres)
+	build_demo_world(&data);
 
 	mlx_set_setting(MLX_MAXIMIZED, false);
 	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "miniRT", false);
@@ -32,7 +68,7 @@ int	main(int ac, char **av)
 	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
 		return (1);
 
-	// Render the gradient
+	// Render
 	i = 0;
 	while (i < HEIGHT)
 	{
@@ -50,15 +86,10 @@ int	main(int ac, char **av)
 			ray.origin = data.camera.cords;
 			ray.direction = ray_direction;
 
-			// Get color from ray
-			t_rgb color = ray_color(&ray);
-
-			// Convert to 0-255 range
-			color.r = (int)(color.r * 255.0);
-			color.g = (int)(color.g * 255.0);
-			color.b = (int)(color.b * 255.0);
-
-			mlx_put_pixel(img, j, i, rgb_to_uint32(&color));
+			// Get color from world
+			t_rgb color = ray_color(&ray, data.objects);
+			uint32_t packed = rgb_to_uint32(&color);
+			mlx_put_pixel(img, j, i, packed);
 			j++;
 		}
 		i++;
