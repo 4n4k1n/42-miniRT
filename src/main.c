b05cad28ac959a19a31a74fc0264c6fcf264b182
Niklas Weber <nweber@student.42Heilbronn.de>
2025-09-30 12:45:51 +0200

    fix: compiling issue

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 11:00:01 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/29 21:35:36 by anakin           ###   ########.fr       */
+/*   Updated: 2025/09/30 12:38:19 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int	main(int ac, char **av)
 	t_data	data;
 	int	i;
 	int	j;
-	
+
 	(void)ac;
 	(void)av;
 
 	// Initialize camera
 	init_camera(&data);
-	
+
 	mlx_set_setting(MLX_MAXIMIZED, false);
 	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "miniRT", false);
 	if (!mlx)
@@ -44,20 +44,20 @@ int	main(int ac, char **av)
 			t_vec3 temp_v = vec3_multiply_inline(&data.camera.pixel_delta_v, i);
 			t_vec3 temp_offset = vec3_add_inline(&temp_u, &temp_v);
 			t_vec3 pixel_center = vec3_add_inline(&data.camera.pixel00_loc, &temp_offset);
-			t_vec3 ray_direction = vec3_sub_inline(&pixel_center, &data.camera.center);
-			
+			t_vec3 ray_direction = vec3_sub_inline(&pixel_center, &data.camera.cords);
+
 			t_ray ray;
-			ray.origin = data.camera.center;
+			ray.origin = data.camera.cords;
 			ray.direction = ray_direction;
-			
+
 			// Get color from ray
 			t_rgb color = ray_color(&ray);
-			
+
 			// Convert to 0-255 range
 			color.r = (int)(color.r * 255.0);
 			color.g = (int)(color.g * 255.0);
 			color.b = (int)(color.b * 255.0);
-			
+
 			mlx_put_pixel(img, j, i, rgb_to_uint32(&color));
 			j++;
 		}
--- src/utils/image.c
+++ src/utils/image.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   image.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 17:53:09 by apregitz          #+#    #+#             */
-/*   Updated: 2025/09/29 19:46:41 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/09/30 12:38:31 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,18 @@ void	init_camera(t_data *data)
 	data->camera.foc = 1.0;
 	data->camera.viewport_height = 2.0;
 	data->camera.viewport_width = data->camera.viewport_height * (WIDTH / HEIGHT);
-	data->camera.center = vec3_init_inline(0.0, 0.0, 0.0);
+	data->camera.cords = vec3_init_inline(0.0, 0.0, 0.0);
 	data->camera.viewport_u = vec3_init_inline(data->camera.viewport_width, 0.0, 0.0);
 	data->camera.viewport_v = vec3_init_inline(0.0, data->camera.viewport_height * -1, 0.0);
 	data->camera.pixel_delta_u = vec3_divide_inline(&data->camera.viewport_u, WIDTH);
 	data->camera.pixel_delta_v = vec3_divide_inline(&data->camera.viewport_v, HEIGHT);
 	t_vec3 temp1 = vec3_init_inline(0.0, 0.0, data->camera.foc);
-	t_vec3 temp2 = vec3_sub_inline(&data->camera.center, &temp1);
+	t_vec3 temp2 = vec3_sub_inline(&data->camera.cords, &temp1);
 	t_vec3 temp3 = vec3_divide_inline(&data->camera.viewport_u, 2.0);
 	t_vec3 temp4 = vec3_sub_inline(&temp2, &temp3);
 	t_vec3 temp5 = vec3_divide_inline(&data->camera.viewport_v, 2.0);
 	data->camera.viewport_upper_left = vec3_sub_inline(&temp4, &temp5);
-	
+
 	t_vec3 temp6 = vec3_add_inline(&data->camera.pixel_delta_u, &data->camera.pixel_delta_v);
 	t_vec3 temp7 = vec3_multiply_inline(&temp6, 0.5);
 	data->camera.pixel00_loc = vec3_add_inline(&data->camera.viewport_upper_left, &temp7);
