8eb87dcbb67a8b4ff11995e58d6989fbbdb8cc82
4n4k1n <anakin@pregitzer.de>
2025-10-01 21:57:58 +0200

    add: header file for the settings

--- inc/mini_rt.h
+++ inc/mini_rt.h
@@ -7,38 +7,17 @@
 # include <unistd.h>
 # include <math.h>
 # include <fcntl.h>
+# include <sys/time.h>
 # include "objects.h"
 # include "camera.h"
 # include "parsing.h"
 # include "own_math.h"
 # include "hittable.h"
 # include "material.h"
+# include "settings.h"
 
 # include "../MLX42/include/MLX42/MLX42.h"
 # include "../libft/libft.h"
-
-# define WIDTH 1920
-# define HEIGHT 1080
-# define ASPECT_RATIO ((double)WIDTH / (double)HEIGHT)
-
-// Adaptive AA configuration
-// Minimum samples per pixel before checking variance
-# ifndef AA_MIN_SAMPLES
-#  define AA_MIN_SAMPLES 256
-# endif
-
-# ifndef AA_MAX_SAMPLES
-#  define AA_MAX_SAMPLES (AA_MIN_SAMPLES * 8)
-# endif
-
-// Variance threshold on normalized luminance [0,1]. stddev ~= sqrt(AA_VAR_EPS)
-# ifndef AA_VAR_EPS
-#  define AA_VAR_EPS 0.0004 /* stddev ~= 0.02 */
-# endif
-
-# define MAX_DEPTH 1024
-
-# define COLOR_INTENSITY 0.5
 
 t_rgb		ray_color(t_ray *ray, t_obj_list *world, int depth);
 uint32_t	rgb_to_uint32(t_rgb *color);
