29eb1365d164385f4571200b87253756dea1bae7
Niklas Weber <nweber@student.42Heilbronn.de>
2025-10-01 15:08:17 +0200

    add: metal as material

---
+++ inc/material.h
@@ -0,0 +1,29 @@
+#ifndef MATERIAL_H
+# define MATERIAL_H
+
+struct s_ray;
+struct s_rgb;
+struct s_hit_record;
+
+typedef struct s_material t_material;
+
+typedef int (*t_scatter_fn)(
+	const t_material* self,
+	const struct s_ray* r_in,
+	const struct s_hit_record* rec,
+	struct s_rgb* attenuation,
+	struct s_ray* scattered
+);
+
+struct s_material
+{
+	t_scatter_fn	scatter;
+	struct s_rgb	albedo;
+	double			fuzz;
+	int				type;
+};
+
+t_material*	material_lambertian(struct s_rgb albedo);
+t_material*	material_metal(struct s_rgb albedo, double fuzz);
+
+#endif
