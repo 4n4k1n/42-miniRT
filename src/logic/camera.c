e36636469550bb560d41a982cd10c96e80127e38
4n4k1n <anakin@pregitzer.de>
2025-10-01 23:20:21 +0200

    update: camera_init function

--- src/logic/camera.c
+++ src/logic/camera.c
@@ -106,27 +106,15 @@ static int	get_image_height(int width, double ra)
  */
 void	init_camera(t_data *data)
 {
-	t_init_tmp tmp;
-
 	data->width = WIDTH;
 	data->height = get_image_height(WIDTH, ASPECT_RATIO);
 	data->camera.foc = 1.0;
 	data->camera.viewport_height = 2.0;
 	data->camera.viewport_width = data->camera.viewport_height * ASPECT_RATIO;
 	data->camera.cords = vec3_init_inline(0.0, 0.0, 0.0);
-	data->camera.viewport_u = vec3_init_inline(data->camera.viewport_width, 0.0, 0.0);
-	data->camera.viewport_v = vec3_init_inline(0.0, data->camera.viewport_height * -1, 0.0);
-	data->camera.pixel_delta_u = vec3_divide_inline(&data->camera.viewport_u, WIDTH);
-	data->camera.pixel_delta_v = vec3_divide_inline(&data->camera.viewport_v, HEIGHT);
-	tmp.t1 = vec3_init_inline(0.0, 0.0, data->camera.foc);
-	tmp.t2 = vec3_sub_inline(&data->camera.cords, &tmp.t1);
-	tmp.t3 = vec3_divide_inline(&data->camera.viewport_u, 2.0);
-	tmp.t4 = vec3_sub_inline(&tmp.t2, &tmp.t3);
-	tmp.t5 = vec3_divide_inline(&data->camera.viewport_v, 2.0);
-	data->camera.viewport_upper_left = vec3_sub_inline(&tmp.t4, &tmp.t5);
-	tmp.t6 = vec3_add_inline(&data->camera.pixel_delta_u, &data->camera.pixel_delta_v);
-	tmp.t7 = vec3_multiply_inline(&tmp.t6, 0.5);
-	data->camera.pixel00_loc = vec3_add_inline(&data->camera.viewport_upper_left, &tmp.t7);
+	data->camera.pitch = 0.0;
+	data->camera.yaw = 0.0;
+	update_camera(data);
 }
 
 
