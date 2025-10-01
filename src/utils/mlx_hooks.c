4c4646efedb009a9077f3895bd1f3c7d9e678d14
4n4k1n <anakin@pregitzer.de>
2025-10-01 23:42:03 +0200

    add: get the vector to move to and then add it onto the camera vector

--- src/utils/mlx_hooks.c
+++ src/utils/mlx_hooks.c
@@ -6,7 +6,7 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 23:21:04 by anakin            #+#    #+#             */
-/*   Updated: 2025/10/01 23:21:59 by anakin           ###   ########.fr       */
+/*   Updated: 2025/10/01 23:35:45 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 void	key_hook(mlx_key_data_t keydata, void *param)
 {
 	t_data	*data;
+	t_vec3	forward, right, up;
+	t_vec3	move_vec;
 
 	data = (t_data *)param;
 	if ((keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT) && (keydata.key == MLX_KEY_W
@@ -29,14 +31,30 @@ void	key_hook(mlx_key_data_t keydata, void *param)
 	{
 		if (keydata.key == MLX_KEY_ESCAPE)
 			mlx_close_window(data->mlx);
-		else if (keydata.key == MLX_KEY_W)
-			data->camera.cords.z -= 0.2;
-		else if (keydata.key == MLX_KEY_S)
-			data->camera.cords.z += 0.2;
-		else if (keydata.key == MLX_KEY_A)
-			data->camera.cords.x -= 0.2;
-		else if (keydata.key == MLX_KEY_D)
-			data->camera.cords.x += 0.2;
+		else if (keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_A || keydata.key == MLX_KEY_D)
+		{
+			get_camera_vectors(data, &forward, &right, &up);
+			if (keydata.key == MLX_KEY_W)
+			{
+				move_vec = vec3_multiply_inline(&forward, -0.2);
+				data->camera.cords = vec3_add_inline(&data->camera.cords, &move_vec);
+			}
+			else if (keydata.key == MLX_KEY_S)
+			{
+				move_vec = vec3_multiply_inline(&forward, 0.2);
+				data->camera.cords = vec3_add_inline(&data->camera.cords, &move_vec);
+			}
+			else if (keydata.key == MLX_KEY_A)
+			{
+				move_vec = vec3_multiply_inline(&right, -0.2);
+				data->camera.cords = vec3_add_inline(&data->camera.cords, &move_vec);
+			}
+			else if (keydata.key == MLX_KEY_D)
+			{
+				move_vec = vec3_multiply_inline(&right, 0.2);
+				data->camera.cords = vec3_add_inline(&data->camera.cords, &move_vec);
+			}
+		}
 		else if (keydata.key == MLX_KEY_SPACE)
 			data->camera.cords.y += 0.2;
 		else if (keydata.key == MLX_KEY_LEFT_SHIFT)
