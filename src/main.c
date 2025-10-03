330a48c0db88944832cfc50d33be172bd8cb2b8e
Niklas Weber <nweber@student.42Heilbronn.de>
2025-10-04 01:21:51 +0200

    update: scene to include planes

--- src/main.c
+++ src/main.c
@@ -19,62 +19,79 @@
  */
 static void	build_demo_world(t_data *data)
 {
-    t_obj	*s1;
-    t_obj	*s2;
-    // NEW demo spheres
-    t_obj	*s3;
-    t_obj	*s4;
+	t_obj	*s1;
+	t_obj	*s3;
+	t_obj	*s4;
+	t_obj	*p_floor;
+	t_obj	*p_back;
+	t_obj	*p_left;
 
-    data->objects = (t_obj_list *)malloc(sizeof(t_obj_list));
-    if (!data->objects)
-        return;
-    data->objects->head = NULL;
-    data->objects->tail = NULL;
-    data->objects->size = 0;
+	data->objects = (t_obj_list *)malloc(sizeof(t_obj_list));
+	if (!data->objects)
+		return;
+	data->objects->head = NULL;
+	data->objects->tail = NULL;
+	data->objects->size = 0;
 
-    // Ground sphere: lambertian (0.8,0.8,0.0)
-    s2 = obj_new(SPHERE);
-    if (s2)
-    {
-        s2->data.sphere.cords = vec3_init_inline(0.0, -100.5, -1.0);
-        s2->data.sphere.diameter = 200.0;
-        s2->data.sphere.rgb = (t_rgb){255, 255, 255};
-        s2->data.sphere.mat = material_metal((t_rgb){204.0, 204.0, 204.0}, 0.0);
-        obj_push(data->objects, s2);
-    }
+	s1 = obj_new(SPHERE);
+	if (s1)
+	{
+		s1->data.sphere.cords = vec3_init_inline(0.0, 0.0, -1.2);
+		s1->data.sphere.diameter = 1.0;
+		s1->data.sphere.rgb = (t_rgb){255, 255, 255};
+		s1->data.sphere.mat = material_metal((t_rgb){204.0, 204.0, 204.0}, 0.0);
+		obj_push(data->objects, s1);
+	}
 
-    // Center sphere: lambertian (0.1,0.2,0.5)
-    s1 = obj_new(SPHERE);
-    if (s1)
-    {
-        s1->data.sphere.cords = vec3_init_inline(0.0, 0.0, -1.2);
-        s1->data.sphere.diameter = 1.0;
-        s1->data.sphere.rgb = (t_rgb){255, 255, 255};
-        s1->data.sphere.mat = material_metal((t_rgb){204.0, 204.0, 204.0}, 0.0);
-        obj_push(data->objects, s1);
-    }
+	s3 = obj_new(SPHERE);
+	if (s3)
+	{
+		s3->data.sphere.cords = vec3_init_inline(-1.0, 0.0, -1.0);
+		s3->data.sphere.diameter = 1.0;
+		s3->data.sphere.rgb = (t_rgb){255, 255, 255};
+		s3->data.sphere.mat = material_metal((t_rgb){204.0, 153.0, 51.0}, 0.0);
+		obj_push(data->objects, s3);
+	}
 
-    // Left sphere: metal (0.8,0.8,0.8), fuzz 0.3
-    s3 = obj_new(SPHERE);
-    if (s3)
-    {
-        s3->data.sphere.cords = vec3_init_inline(-1.0, 0.0, -1.0);
-        s3->data.sphere.diameter = 1.0;
-        s3->data.sphere.rgb = (t_rgb){255, 255, 255};
-        s3->data.sphere.mat = material_metal((t_rgb){204.0, 153.0, 51.0}, 0.0);
-        obj_push(data->objects, s3);
-    }
+	s4 = obj_new(SPHERE);
+	if (s4)
+	{
+		s4->data.sphere.cords = vec3_init_inline(1.0, 0.0, -1.0);
+		s4->data.sphere.diameter = 1.0;
+		s4->data.sphere.rgb = (t_rgb){255, 255, 255};
+		s4->data.sphere.mat = material_metal((t_rgb){204.0, 153.0, 51.0}, 0.0);
+		obj_push(data->objects, s4);
+	}
 
-    // Right sphere: metal (0.8,0.6,0.2), fuzz 1.0
-    s4 = obj_new(SPHERE);
-    if (s4)
-    {
-        s4->data.sphere.cords = vec3_init_inline(1.0, 0.0, -1.0);
-        s4->data.sphere.diameter = 1.0;
-        s4->data.sphere.rgb = (t_rgb){255, 255, 255};
-        s4->data.sphere.mat = material_metal((t_rgb){204.0, 153.0, 51.0}, 0.0);
-        obj_push(data->objects, s4);
-    }
+	p_floor = obj_new(PLANE);
+	if (p_floor)
+	{
+		p_floor->data.plane.cords = vec3_init_inline(0.0, -0.5, 0.0);
+		p_floor->data.plane.norm = vec3_init_inline(0.0, 1.0, 0.0);
+		p_floor->data.plane.rgb = (t_rgb){200, 200, 200};
+		p_floor->data.plane.mat = material_metal((t_rgb){200.0, 200.0, 200.0}, 0.0);
+		obj_push(data->objects, p_floor);
+	}
+
+	p_back = obj_new(PLANE);
+	if (p_back)
+	{
+		p_back->data.plane.cords = vec3_init_inline(0.0, 0.0, -5.0);
+		p_back->data.plane.norm = vec3_init_inline(0.0, 0.0, 1.0);
+		p_back->data.plane.rgb = (t_rgb){180, 180, 255};
+		p_back->data.plane.mat = material_metal((t_rgb){180.0, 180.0, 255.0}, 0.0);
+		obj_push(data->objects, p_back);
+	}
+
+	p_left = obj_new(PLANE);
+	if (p_left)
+	{
+		p_left->data.plane.cords = vec3_init_inline(-2.0, 0.0, -1.0);
+		p_left->data.plane.norm = vec3_init_inline(1.0, 0.0, 0.0);
+		p_left->data.plane.rgb = (t_rgb){255, 200, 200};
+		p_left->data.plane.mat = material_metal((t_rgb){255.0, 200.0, 200.0}, 0.0);
+		obj_push(data->objects, p_left);
+	}
 }
 
 int	get_time_in_ms(void)
@@ -98,6 +115,8 @@ void	cleanup_objects(t_obj_list *objects)
 		next = current->next;
 		if (current->type == SPHERE && current->data.sphere.mat)
 			free(current->data.sphere.mat);
+		if (current->type == PLANE && current->data.plane.mat)
+			free(current->data.plane.mat);
 		free(current);
 		current = next;
 	}
@@ -116,7 +135,7 @@ int	main(int ac, char **av)
 
 	(void)ac;
 	(void)av;
-    data.aa_state = ANTI_ALIASING;
+	data.aa_state = ANTI_ALIASING;
 	data.camera.samples_per_pixel = AA_MAX_SAMPLES;
 	init_camera(&data);
 	build_demo_world(&data);
