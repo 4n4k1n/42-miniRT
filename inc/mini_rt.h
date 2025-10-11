fa606be890278d8b15dde95d0f452dc1cfdedb85
Anakin Pregitzer <anakin@pregitzer.de>
2025-10-11 12:47:57 +0200

    add: error function

--- inc/mini_rt.h
+++ inc/mini_rt.h
@@ -10,6 +10,8 @@
 # include <sys/time.h>
 # include <threads.h>
 # include <pthread.h>
+# include <sys/socket.h>
+# include <sys/stat.h>
 # include "objects.h"
 # include "camera.h"
 # include "parsing.h"
@@ -36,5 +38,6 @@ void	*thread_job(void *arg);
 int	render_with_mt(t_data *data);
 void	cleanup_data(t_data *data);
 int	init_threads(t_data *data);
+int ft_error(char *msg, int error_code);
 
 #endif
