6505c1fbfd8276918d8484255c6c4ce78a1ba0c7
Anakin Pregitzer <anakin@pregitzer.de>
2025-10-12 16:04:11 +0200

    add: functio to copie the content of the tile in to the mxl image

--- src/server/worker.c
+++ src/server/worker.c
@@ -6,7 +6,7 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 13:35:40 by anakin            #+#    #+#             */
-/*   Updated: 2025/10/12 14:41:18 by anakin           ###   ########.fr       */
+/*   Updated: 2025/10/12 16:03:20 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,33 @@ void    *worker_thread_func(void *arg)
     }
     close(context->worker_socket);
     return (NULL);
+}
+
+void copy_tile_to_framebuffer(mlx_image_t *image, t_tile *result, uint32_t *pixels)
+{
+    uint32_t    y;
+    uint32_t    x;
+    uint32_t img_x;
+    uint32_t img_y;
+    uint32_t pixel_idx;
+    uint32_t img_idx;
+
+    y = 0;
+    while (y < result->height)
+    {
+        x = 0;
+        while (x < result->width)
+        {
+            img_x = result->x + x;
+            img_y = result->y + y;
+            pixel_idx = y * result->width + x;
+            img_idx = img_y * image->width + img_x;
+            image->pixels[img_idx * 4 + 0] = (pixels[pixel_idx] >> 24) & 0xFF;
+            image->pixels[img_idx * 4 + 1] = (pixels[pixel_idx] >> 16) & 0xFF;
+            image->pixels[img_idx * 4 + 2] = (pixels[pixel_idx] >> 8) & 0xFF;
+            image->pixels[img_idx * 4 + 3] = pixels[pixel_idx] & 0xFF;
+            x++;
+        }
+        y++;
+    }
 }
