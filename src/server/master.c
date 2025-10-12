e76294b310bc1e3e2d4bd5ca17477ead50e56455
Anakin Pregitzer <anakin@pregitzer.de>
2025-10-12 13:25:38 +0200

    add: function to setup the master and checks if the img is rendered.

--- src/server/master.c
+++ src/server/master.c
@@ -6,7 +6,7 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 11:41:09 by anakin            #+#    #+#             */
-/*   Updated: 2025/10/12 12:05:43 by anakin           ###   ########.fr       */
+/*   Updated: 2025/10/12 13:24:17 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,14 @@ int run_master(char *scene_file, uint32_t port)
     t_data      data;
 
     setup_master(&master, &data, scene_file, port);
+    while (!master.shutdown)
+    {
+        pthread_mutex_lock(&master.queue->lock);
+        if (master.queue->current >= master.queue->size)
+            master.shutdown = true;
+        pthread_mutex_unlock(&master.queue->lock);
+    }
+    destroy_queue(master.queue);
+    mlx_terminate(master.mlx);
+    return (0);
 }
