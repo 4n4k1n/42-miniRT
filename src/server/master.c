ae29709429596291ad56d14e4e347d4fa642bd04
Anakin Pregitzer <anakin@pregitzer.de>
2025-10-16 13:16:18 +0200

    add: protection for system calls in the setup_master function, debug info in the run_master and a phase where the workers can connect t othe master.

--- src/server/master.c
+++ src/server/master.c
@@ -16,30 +16,76 @@ static int setup_master(t_master *master, t_data *data, char *scene_file, uint32
 {
     ft_memset(master, 0, sizeof(t_master));
     master->scene_file = scene_file;
+    master->start_render = false;
     parse_scene(scene_file, data);
-    init_queue(&master->queue, WIDTH, HEIGHT, TILE_SIZE);
+    master->queue = malloc(sizeof(t_queue));
+    if (!master->queue)
+        return (ft_error("malloc", 1));
+    init_queue(master->queue, WIDTH, HEIGHT, TILE_SIZE);
     master->mlx = mlx_init(WIDTH, HEIGHT, "miniRT", false);
+    if (!master->mlx)
+        return (ft_error("mlx_init", 1));
     master->img = mlx_new_image(master->mlx, WIDTH, HEIGHT);
+    if (!master->img)
+        return (ft_error("mlx_new_image", 1));
     mlx_image_to_window(master->mlx, master->img, 0, 0);
     pthread_mutex_init(&master->img_lock, NULL);
     master->socket_fd = setup_listen_socket(port);
-    pthread_create(&master->accept_thread, NULL, accept_worker_thread, &master);
+    if (master->socket_fd < 0)
+        return (1);
+    pthread_create(&master->accept_thread, NULL, accept_worker_threads, master);
+    return (0);
 }
 
 int run_master(char *scene_file, uint32_t port)
 {
     t_master    master;
     t_data      data;
+    char        buffer[10];
+    int         prev_workers;
 
-    setup_master(&master, &data, scene_file, port);
-    while (!master.shutdown)
+    if (setup_master(&master, &data, scene_file, port) != 0)
+        return (1);
+    printf("\n=== Master Server Ready ===\n");
+    printf("Scene: %s\n", scene_file);
+    printf("Resolution: %dx%d\n", (int)WIDTH, (int)HEIGHT);
+    printf("Tile size: %d\n", TILE_SIZE);
+    printf("Total tiles: %zu\n\n", master.queue->size);
+    printf("Waiting for workers to connect...\n");
+    printf("Press ENTER to start rendering\n\n");
+    prev_workers = 0;
+    while (1)
     {
-        pthread_mutex_lock(&master.queue->lock);
-        if (master.queue->current >= master.queue->size)
-            master.shutdown = true;
-        pthread_mutex_unlock(&master.queue->lock);
+        fd_set readfds;
+        struct timeval tv;
+        int ret;
+
+        if (master.num_worker != prev_workers)
+        {
+            printf("Workers connected: %d\n", master.num_worker);
+            prev_workers = master.num_worker;
+        }
+        FD_ZERO(&readfds);
+        FD_SET(STDIN_FILENO, &readfds);
+        tv.tv_sec = 0;
+        tv.tv_usec = 100000;
+        ret = select(STDIN_FILENO + 1, &readfds, NULL, NULL, &tv);
+        if (ret > 0 && FD_ISSET(STDIN_FILENO, &readfds))
+        {
+            if (fgets(buffer, sizeof(buffer), stdin) != NULL && buffer[0] == '\n')
+                break;
+        }
     }
+    printf("\n=== Starting Render ===\n");
+    printf("Workers: %d\n", master.num_worker);
+    printf("Tiles per worker: ~%zu\n\n", master.queue->size / (master.num_worker > 0 ? master.num_worker : 1));
+    master.start_render = true;
+    mlx_loop(master.mlx);
+    master.shutdown = true;
+    pthread_join(master.accept_thread, NULL);
     destroy_queue(master.queue);
+    free(master.queue);
     mlx_terminate(master.mlx);
+    free_scene(&data);
     return (0);
 }
