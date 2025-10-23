f791882d1881b67fc6ea07335805922527b7dd97
Anakin Pregitzer <anakin@pregitzer.de>
2025-10-23 14:31:44 +0200

    fix: wrong varible byte oder convertion

--- src/server/send/send_tile_result.c
+++ src/server/send/send_tile_result.c
@@ -20,7 +20,7 @@ void    send_tile_result(int socket_fd, t_tile *tile_result, uint32_t *pixels)
     pixel_bytes = tile_result->height * tile_result->width * sizeof(uint32_t);
     send_header(socket_fd, MSG_TILE_COMPLETE, sizeof(t_tile) + pixel_bytes);
     net_tile.height = htonl(tile_result->height);
-    net_tile.height = htonl(tile_result->tile_id);
+    net_tile.tile_id = htonl(tile_result->tile_id);
     net_tile.width = htonl(tile_result->width);
     net_tile.x = htonl(tile_result->x);
     net_tile.y = htonl(tile_result->y);
