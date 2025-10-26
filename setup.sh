14b0f907d27cc4c98be5e4f3aaabdc56c9989d57
Anakin Pregitzer <anakin@pregitzer.de>
2025-10-26 15:29:56 +0100

    update: setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-git clone https://github.com/4n4k1n/42-miniRT.git ~/miniRT
 
-if [ $# -eq 0 ]; then
-    read -p "Enter IP address: " ip_address
+if [ -d ~/miniRT ]; then
+    echo "Repository already exists at ~/miniRT"
 else
-    rm -rf ~/miniRT
-    exit 1
+    git clone https://github.com/4n4k1n/42-miniRT.git ~/miniRT
+fi
+
+if [ -z "$IP_ADDRESS" ]; then
+    read -p "Enter IP address: " ip_address
+    export IP_ADDRESS="$ip_address"
+else
+    echo "Using IP_ADDRESS from environment: $IP_ADDRESS"
+    ip_address="$IP_ADDRESS"
 fi
 
 cd ~/miniRT
 git checkout 60-multi-server-rendering
 make
 ~/miniRT/miniRT --worker $ip_address
-rm -rf ~/miniRT
