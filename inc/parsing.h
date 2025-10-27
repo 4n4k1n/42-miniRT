a2135c3c5dd83d909b56f5ec7144bb3e4a0157ba
Niklas Weber <nweber@student.42Heilbronn.de>
2025-10-27 11:30:02 +0100

    Adjust: parsing for pyramid

--- inc/parsing.h
+++ inc/parsing.h
@@ -23,6 +23,7 @@ int		parse_light(char **tokens, t_data *scene, t_arg_check *args);
 int		parse_cylinder(char **tokens, t_data *scene);
 int		parse_plane(char **tokens, t_data *scene);
 int		parse_sphere(char **tokens, t_data *scene);
+int		parse_pyramid(char **tokens, t_data *scene);
 
 // Utils
 int		is_rt_file(const char *path);
--- src/parsing/validation_obj.c
+++ src/parsing/validation_obj.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validation_obj.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 10:36:19 by nweber            #+#    #+#             */
-/*   Updated: 2025/10/05 12:21:58 by anakin           ###   ########.fr       */
+/*   Updated: 2025/10/27 11:27:13 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,5 +117,41 @@ int	parse_cylinder(char **tokens, t_data *scene)
 		return (free(o), rt_error("invalid cylinder material"));
 	if (obj_push(scene->objects, o))
 		return (free(o), rt_error("object push failed"));
+	return (0);
+}
+
+int	parse_pyramid(char **tokens, t_data *scene)
+{
+	t_obj	*o;
+	int		len;
+
+	len = ft_arrlen(tokens);
+	if (len != 6 && len != 7)
+		return (rt_error("invalid pyramid format"));
+	o = obj_new(PYRAMID);
+	if (!o)
+		return (rt_error("malloc failed (pyramid)"));
+	if (parse_vec3(tokens[1], &o->data.pyramid.cords))
+		return (free(o), rt_error("invalid pyramid center"));
+	if (parse_vec3(tokens[2], &o->data.pyramid.norm)
+		|| !vec_in_range(&o->data.pyramid.norm, -1.0, 1.0)
+		|| !vec_non_zero(o->data.pyramid.norm))
+		return (free(o), rt_error("invalid pyramid axis"));
+	if (parse_double(tokens[3], &o->data.pyramid.base)
+		|| o->data.pyramid.base <= 0.0)
+		return (free(o), rt_error("invalid pyramid base"));
+	if (parse_double(tokens[4], &o->data.pyramid.height)
+		|| o->data.pyramid.height <= 0.0)
+		return (free(o), rt_error("invalid pyramid height"));
+	if (parse_rgb(tokens[5], &o->data.pyramid.rgb))
+		return (free(o), rt_error("invalid pyramid RGB"));
+	if (parse_material(tokens, len, o))
+		return (free(o), rt_error("invalid pyramid material"));
+	if (obj_push(scene->objects, o))
+	{
+		if (o->data.pyramid.mat)
+			free(o->data.pyramid.mat);
+		return (free(o), rt_error("obj push failed"));
+	}
 	return (0);
 }
