f6831cfd128b7d4a534f6a91f66dcae93f670472
Anakin Pregitzer <anakin@pregitzer.de>
2025-10-28 21:56:32 +0100

    add: triangle to the hit functions

--- src/logic/hittable.c
+++ src/logic/hittable.c
@@ -36,6 +36,8 @@ int	hittable_hit(const t_obj *o, t_ray *r, double min, double max, t_hit_record 
 		return (hit_pyramid_obj(&o->data.pyramid, r, min, max, rec));
 	if (o->type == CONE)
 		return (hit_cone_obj(&o->data.cone, r, min, max, rec));
+	if (o->type == TRIANGLE)
+		return (hit_triangle_obj(&o->data.triangle, r, min, max, rec));
 	return (0);
 }
 
