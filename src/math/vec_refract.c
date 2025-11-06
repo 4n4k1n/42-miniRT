1d34cae8cf9a50b624a60a299fc6d40174b30193
Niklas Weber <nweber@student.42Heilbronn.de>
2025-11-06 12:24:21 +0100

    fixed: norm in math functions

--- src/math/vec_refract.c
+++ src/math/vec_refract.c
@@ -16,16 +16,16 @@ t_vec3	vec3_refract(const t_vec3 v, const t_vec3 n, double etai_over_etat)
 {
 	t_vec3	uv;
 	double	cos_theta;
-	t_vec3	r_out_perp;
-	double	k;
-	double	c;
 	t_vec3	r_out_parallel;
 
 	uv = vec3_normalize(v);
 	cos_theta = fmin(vec3_dot(vec3_overload(uv), n), 1.0);
-	r_out_perp = vec3_multiply(vec3_add(uv, vec3_multiply(n, cos_theta)), etai_over_etat);
-	k = 1.0 - etai_over_etat * etai_over_etat * (1.0 - cos_theta * cos_theta);
-	c = (k > 0.0) ? sqrt(k) : 0.0;
-	r_out_parallel = vec3_multiply(n, -c);
-	return (vec3_add(r_out_perp, r_out_parallel));
+	if (1.0 - etai_over_etat * etai_over_etat
+		* (1.0 - cos_theta * cos_theta) > 0.0)
+		r_out_parallel = vec3_multiply(n, -sqrt(1.0 - \
+			etai_over_etat * etai_over_etat * (1.0 - cos_theta * cos_theta)));
+	else
+		r_out_parallel = vec3_multiply(n, 0.0);
+	return (vec3_add(vec3_multiply(vec3_add(uv, \
+		vec3_multiply(n, cos_theta)), etai_over_etat), r_out_parallel));
 }
