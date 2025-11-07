7b6c90f6584a656c92964d92967c9b419b21520a
Anakin Pregitzer <4n4k1n>
2025-11-07 10:47:22 +0100

    fix: norm for the recive functions

--- src/server/recive/recive_all.c
+++ src/server/recive/recive_all.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   recive_all.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: claude <claude@student.42.fr>              +#+  +:+       +#+        */
+/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 00:00:00 by claude            #+#    #+#             */
-/*   Updated: 2025/10/24 00:00:00 by claude           ###   ########.fr       */
+/*   Updated: 2025/11/07 10:45:16 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,23 @@
  * Loops until the entire buffer is filled or an error occurs.
  * Returns 0 on success, -1 on error.
  */
-int recv_all(int socket_fd, void *buffer, size_t length)
+int	recv_all(int socket_fd, void *buffer, size_t length)
 {
-    char        *ptr;
-    size_t      remaining;
-    ssize_t     received;
+	char	*ptr;
+	size_t	remaining;
+	ssize_t	received;
 
-    ptr = (char *)buffer;
-    remaining = length;
-    while (remaining > 0)
-    {
-        received = recv(socket_fd, ptr, remaining, MSG_WAITALL);
-        if (received < 0)
-            return (-1);
-        if (received == 0)
-            return (-1);
-        ptr += received;
-        remaining -= received;
-    }
-    return (0);
+	ptr = (char *)buffer;
+	remaining = length;
+	while (remaining > 0)
+	{
+		received = recv(socket_fd, ptr, remaining, MSG_WAITALL);
+		if (received < 0)
+			return (-1);
+		if (received == 0)
+			return (-1);
+		ptr += received;
+		remaining -= received;
+	}
+	return (0);
 }
--- src/server/recive/recive_tile_assignment.c
+++ src/server/recive/recive_tile_assignment.c
@@ -6,21 +6,21 @@
 /*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 22:20:27 by anakin            #+#    #+#             */
-/*   Updated: 2025/10/23 14:32:02 by anakin           ###   ########.fr       */
+/*   Updated: 2025/11/07 10:45:32 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
 
-t_tile  recive_tile_assignment(int socket_fd)
+t_tile	recive_tile_assignment(int socket_fd)
 {
-    t_tile          tile;
+	t_tile	tile;
 
-    recv_all(socket_fd, &tile, sizeof(t_tile));
-    tile.height = ntohl(tile.height);
-    tile.tile_id = ntohl(tile.tile_id);
-    tile.width = ntohl(tile.width);
-    tile.x = ntohl(tile.x);
-    tile.y = ntohl(tile.y);
-    return (tile);
+	recv_all(socket_fd, &tile, sizeof(t_tile));
+	tile.height = ntohl(tile.height);
+	tile.tile_id = ntohl(tile.tile_id);
+	tile.width = ntohl(tile.width);
+	tile.x = ntohl(tile.x);
+	tile.y = ntohl(tile.y);
+	return (tile);
 }
