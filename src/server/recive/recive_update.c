30dc871718208aa06475ef82e7669def5be3240d
Niklas Weber <nweber@student.42Heilbronn.de>
2025-11-12 16:44:05 +0100

    update: enhance function documentation

--- src/server/recive/recive_update.c
+++ src/server/recive/recive_update.c
@@ -3,15 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   recive_update.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 18:51:58 by apregitz          #+#    #+#             */
-/*   Updated: 2025/10/13 18:55:32 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/11/12 16:43:51 by nweber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
 
+/**
+ * Receive an update message from the socket.
+ * Reads the message header and expects MSG_UPDATE. Then reads the update
+ * payload into a t_update structure and converts fields from network to
+ * host byte order where necessary before returning it.
+ * @param socket_fd connected socket file descriptor
+ * @return fully populated t_update (caller receives by value)
+ */
 t_update	recive_update(int socket_fd)
 {
 	t_msg_header	header;
